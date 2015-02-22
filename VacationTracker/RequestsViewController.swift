f1a3728cb14e3856859a6805df466da62ac6346d
Kim D Barnes <kimdbarnes@gmail.com>
2015-02-22 09:36:30 -0700

    Start of adding requests table view

--- VacationTracker/RequestsViewController.swift
+++ VacationTracker/RequestsViewController.swift
@@ -2,12 +2,27 @@ import UIKit
 
 class RequestsViewController: UIViewController {
 
+  @IBOutlet weak var requestTableView: UITableView!
+
+
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
+  }
+}
+
+extension RequestsViewController: UITableViewDataSource, UITableViewDelegate {
+
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 3
+  }
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell:UITableViewCell = self.requestTableView.dequeueReusableCellWithIdentifier("requestRow") as UITableViewCell
+    return cell
   }
 }
 
